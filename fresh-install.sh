021752a639cff3328cbe1a9abeb1a498545cfba6
Patricio Marrone <pmarrone@unc.edu.ar>
2015-08-20 10:15:54 -0300

    Fixed permission issues on dspace deployment directory and mod_jk
    Dspace demployment directory is now created as root and then its
    ownership is given to tomcat7 user and group.
    mod_jk now works correctly.

--- fresh-install.sh
+++ fresh-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Execute ansible install group of tasks
-ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --user=vagrant --private-key=.vagrant/machines/default/virtualbox/private_key site.yml --tags install -v --extra-vars="install=true"
+ansible-playbook -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --user=vagrant --private-key=.vagrant/machines/default/virtualbox/private_key site.yml --tags install -vvvv --extra-vars="install=true"
